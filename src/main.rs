4c2f885e5254472326b1450fcfaebfb8a26f1601
KallelGaNewk <kallelgn@gmail.com>
2023-06-21 00:10:38 -0300

    transmit to other speaker

--- src/main.rs
+++ src/main.rs
@@ -10,54 +10,98 @@ use cpal::{
 };
 
 fn main() {
-    // get all speakers device
     let host = cpal::default_host();
-    let speaker = prompt_speaker(&host);
-    let speaker_config = speaker.default_output_config().unwrap().config();
+
+    let outputspeaker = prompt_speaker(&host);
+    let inputspeaker = prompt_speaker(&host);
+
+    print!("\x1B[2J\x1B[1;1H");
+
+    if outputspeaker.name().unwrap() == inputspeaker.name().unwrap() {
+        println!("Output and input speakers cannot be the same.");
+        return;
+    }
+
+    let outputspeaker_config = outputspeaker.default_output_config().unwrap().config();
+    let inputspeaker_config = inputspeaker.default_output_config().unwrap().config();
 
     println!(
-        "Using speaker: {:?}, ({} channels, {} Hz)",
-        speaker.name().unwrap(),
-        speaker_config.channels,
-        speaker_config.sample_rate.0
+        "Output speaker: \"\x1b[32m{}\x1b[0m\" ({} channels, {} Hz)",
+        outputspeaker.name().unwrap(),
+        outputspeaker_config.channels,
+        outputspeaker_config.sample_rate.0
     );
 
-    let mut writer = hound::WavWriter::create(
-        "output.wav",
-        hound::WavSpec {
-            channels: speaker_config.channels as u16,
-            sample_rate: speaker_config.sample_rate.0,
-            bits_per_sample: 16,
-            sample_format: hound::SampleFormat::Int,
-        },
-    )
-    .unwrap();
+    println!(
+        "Input speaker: \"\x1b[33m{}\x1b[0m\" ({} channels, {} Hz)",
+        inputspeaker.name().unwrap(),
+        inputspeaker_config.channels,
+        inputspeaker_config.sample_rate.0
+    );
+
+    if outputspeaker_config.sample_rate != inputspeaker_config.sample_rate {
+        println!("\x1b[31mSample rate of output and input speakers must be the same.\x1b[0m");
+        println!(
+            "\x1b[31mBoth speakers must be {} Hz or {} Hz.\x1b[0m",
+            outputspeaker_config.sample_rate.0, inputspeaker_config.sample_rate.0
+        );
+        return;
+    }
 
     let audiochannel = channel();
+    let error_callback = move |err| {
+        println!("\x1b[31m{}\x1b[0m", err);
+        std::process::exit(1);
+    };
 
-    let stream = speaker
+    let stream = outputspeaker
         .build_input_stream(
-            &speaker_config,
+            &outputspeaker_config,
             move |data: &[f32], _: &_| {
-                for &sample in data {
-                    writer
-                        .write_sample((sample * i16::MAX as f32) as i16)
-                        .unwrap();
+                let mut output_data = Vec::new();
 
-                    audiochannel.0.send(sample).unwrap();
+                if outputspeaker_config.channels != inputspeaker_config.channels {
+                    for chunk in data.chunks(outputspeaker_config.channels as usize) {
+                        for i in 0..(inputspeaker_config.channels) {
+                            if let Some(&first_value) = chunk.get(i as usize) {
+                                output_data.push(first_value);
+                            }
+                        }
+                    }
+                } else {
+                    output_data.extend_from_slice(data);
                 }
 
-                writer.flush().unwrap();
+                for &sample in &output_data {
+                    audiochannel.0.send(sample).unwrap();
+                }
             },
-            move |err| {
-                println!("an error occurred on stream: {}", err);
-            },
+            error_callback,
             Some(Duration::from_secs(10)),
         )
         .unwrap();
 
     stream.play().unwrap();
 
+    let inputstream = inputspeaker
+        .build_output_stream(
+            &inputspeaker_config,
+            move |data: &mut [f32], _: &_| {
+                for sample in data.iter_mut() {
+                    *sample = audiochannel.1.recv().unwrap();
+                }
+            },
+            error_callback,
+            Some(Duration::from_secs(10)),
+        )
+        .unwrap();
+
+    inputstream.play().unwrap();
+
+    handle_ctrlc();
+}
+
+fn handle_ctrlc() {
     let (tx, rx) = channel();
 
     ctrlc::set_handler(move || tx.send(()).expect("Could not send signal on channel."))
@@ -74,37 +118,31 @@ fn prompt_speaker(host: &Host) -> Device {
         .expect("no output device available")
         .collect::<Vec<_>>();
 
-    // get the default output device
     let default_speaker = host
         .default_output_device()
         .expect("no output device available");
 
-    // get index of default speaker, of the list of all speakers
     let default_speaker_index = all_speakers
         .iter()
         .position(|speaker| speaker.name().unwrap() == default_speaker.name().unwrap())
         .unwrap();
 
-    // print all speakers device
     for (speaker_index, speaker) in all_speakers.iter().enumerate() {
         println!("{}: {}", speaker_index, speaker.name().unwrap());
     }
 
-    println!("");
-
-    // prompt user to choose a speaker
     let input = prompt(&format!(
-        "Choose a speaker (default: {}): ",
+        "\x1b[36mChoose a speaker\x1b[0m [{}]: ",
         default_speaker_index
     ));
 
-    // get the speaker index
+    println!("");
+
     let speaker_index = input
         .trim()
         .parse::<usize>()
         .unwrap_or(default_speaker_index);
 
-    // get the speaker
     host.output_devices()
         .unwrap()
         .nth(speaker_index)
